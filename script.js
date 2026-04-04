86d2ab0535333a35c43e1a33835a50feffccf7e5
itschool <itschool@tou.edu.kz>
2026-04-04 14:49:17 +0500

    init

---
+++ script.js
@@ -0,0 +1,61 @@
+/*
+    let name = "John";
+    name = prompt("Как ваше имя?");
+    alert("Привет, " + name + "!");
+*/
+
+// let a = prompt("Введите первое число:");
+// let b = prompt("Введите второе число:");
+
+// a = Number(a);
+// b = Number(b);
+
+// alert(Number(a) + Number(b));
+// alert(a + b);
+
+// Создание константы
+// const pi = 3.14;
+// pi = 3.15;
+
+// let a = pi * 2 * 5.14;
+
+// console.log(a);
+
+// let a = 19; // 16
+// let b = 4;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a ** b);
+// console.log(a % b);
+// let result = (a + b) * 2 / a;
+// console.log(result);
+
+// let is_admin = true;
+// let dark_theme = false;
+
+// console.log("true && true = " + (true && true));
+// console.log("true && false = " + (true && false));
+// console.log("false && true = " + (false && true));
+// console.log("false && false = " + (false && false));
+
+// console.log("true || true = " + (true || true));
+// console.log("true || false = " + (true || false));
+// console.log("false || true = " + (false || true));
+// console.log("false || false = " + (false || false));
+
+console.log("!true = " + (!true));
+console.log("!false = " + (!false));
+
+console.log(100 > 5);
+console.log(100 >= 5);
+console.log(100 < 5);
+console.log(100 <= 5);
+
+console.log(100 == "100");
+console.log(100 === "100");
+
+console.log(100 != "99");
+console.log(100 !== "100");
